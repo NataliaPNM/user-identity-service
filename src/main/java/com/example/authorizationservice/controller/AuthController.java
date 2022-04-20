42a411e88faba761694c35276385cd7882bfd4a4
Natali <ttnpnm@yandex.ru>
2022-04-20 23:03:55 +0500

    create user-identity-service

--- src/main/java/com/example/authorizationservice/controller/AuthController.java
+++ src/main/java/com/example/authorizationservice/controller/AuthController.java
@@ -13,29 +13,32 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-        private final AuthService authService;
+    private final AuthService authService;
 
-        @PostMapping("/signin")
-        public LoginResponseDto authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-            return authService.login(loginRequest);
-        }
-
-        @PostMapping("/refresh")
-        public LoginResponseDto refreshToken(@RequestHeader(AUTHORIZATION) String refreshToken) {
-            return authService.refreshJwt(refreshToken);
-        }
-        @PostMapping("/secure")
-        @ResponseStatus(HttpStatus.OK)
-        public void changeCredentials(@Valid @RequestBody ChangePasswordRequest changePasswordRequest,
-                                                  @AuthenticationPrincipal JwtUser user) {
-             authService.changePassword(changePasswordRequest,user.getId());
-        }
-
+    @PostMapping("/signin")
+    @ResponseStatus(HttpStatus.OK)
+    public LoginResponseDto authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
     }
+
+    @PostMapping("/refresh")
+    public LoginResponseDto refreshToken(@RequestHeader(AUTHORIZATION) String refreshToken) {
+        return authService.refreshJwt(refreshToken);
+    }
+
+    @PostMapping("/secure")
+    @ResponseStatus(HttpStatus.OK)
+    public void changeCredentials(@Valid @RequestBody ChangePasswordRequest changePasswordRequest,
+                                  @AuthenticationPrincipal JwtUser user) {
+        authService.changePassword(changePasswordRequest, user.getId());
+    }
+
+}
 

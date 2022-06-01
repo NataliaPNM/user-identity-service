861df0e5aad966610fe3865250a2ca51f7e036b5
Natali <ttnpnm@yandex.ru>
2022-06-01 18:09:16 +0500

    MRMN-83/ MR-fix №5/fix bug

--- src/main/java/com/example/repository/CredentialsRepository.java
+++ src/main/java/com/example/repository/CredentialsRepository.java
@@ -2,6 +2,7 @@ package com.example.repository;
 
 import com.example.model.Credentials;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,9 @@ import java.util.UUID;
 @Repository
 public interface CredentialsRepository extends JpaRepository<Credentials, UUID> {
   Optional<Credentials> findByLogin(String login);
+
+  @Query("select c from Credentials c  where c.person.personId =:person")
+  Optional<Credentials> findByPersonId(UUID person);
 
   Optional<Credentials> findByRefreshToken(String refreshToken);
 }

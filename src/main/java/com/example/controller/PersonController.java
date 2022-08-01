dd714740a7284d1f2434f7bfe137225be57246a4
JinQuiet <Quester82@gmail.com>
2022-08-01 19:27:43 +0300

    Additional Swagger docs for PersonController

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -5,9 +5,13 @@ import com.example.dto.request.SetDefaultNotificationTypeRequest;
 import com.example.dto.response.CreatePersonResponseDto;
 import com.example.dto.response.DefaultConfirmationTypeResponse;
 import com.example.dto.response.SetDefaultConfirmationTypeResponse;
+import com.example.dto.response.error.DefaultConfirmationTypeLockedErrorResponse;
+import com.example.dto.response.error.IncorrectDefaultNotificationTypeErrorResponse;
+import com.example.dto.response.error.NotFoundErrorResponse;
 import com.example.service.AccountService;
 import com.example.service.NotificationSettingsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -26,126 +30,86 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RefreshScope
 public class PersonController {
-  private final AccountService accountService;
+    private final AccountService accountService;
   private final NotificationSettingsService notificationSettingsService;
 
-  @Operation(summary = "Returns the default way to send a confirmation code for a specific user")
-  @GetMapping("/defaultConfirmationType")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "OK",
-            content =
-                @Content(
-                    schema =
-                        @Schema(
-                            example = "{\n" + "  \"defaultConfirmationType\": email\"\n" + "}\n"))),
-        @ApiResponse(
-            responseCode = "422",
-            description = "Invalid user id",
-            content =
-                @Content(
-                    schema =
-                        @Schema(
-                            example =
-                                "{\n"
-                                    + "    \"status\", \"422\",\n"
-                                    + "    \"error\", \"Unprocessable Entity\",\n"
-                                    + "    \"personId\", \"Not found person with this login\"\t\n"
-                                    + "}\n")))
-      })
-  public DefaultConfirmationTypeResponse getDefaultType(String personId) {
+    @Operation(summary = "Get default confirmation type",
+            description = "Returns the default way to send a confirmation code for a specific user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "OK",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DefaultConfirmationTypeResponse.class))),
+            @ApiResponse(responseCode = "422",
+                    description = "Invalid user id",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = NotFoundErrorResponse.class)))
+    })
+    @GetMapping("/defaultConfirmationType")
+    public DefaultConfirmationTypeResponse getDefaultType(String personId) {
 
-    return notificationSettingsService.getPersonDefaultNotificationType(UUID.fromString(personId));
-  }
+        return notificationSettingsService.getPersonDefaultNotificationType(UUID.fromString(personId));
+    }
 
-  @Operation(summary = "Change the default way to send the confirmation code")
+  @Operation(summary = "Set default confirmation type",
+  description = "Change the default way to send the confirmation code")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200",
+                  description = "The default way to send a confirmation code has been changed. " +
+                          "Returns E-Mail of a person the code has been sent to.",
+                  content = @Content(
+                          mediaType = "application/json",
+                          schema = @Schema(implementation = SetDefaultConfirmationTypeResponse.class))),
+          @ApiResponse(responseCode = "422",
+                  description = "Invalid operation id",
+                  content =
+                  @Content(
+                          mediaType = "application/json",
+                          // "message": "Not found operation with this id"
+                          schema = @Schema(implementation = NotFoundErrorResponse.class))),
+          @ApiResponse(responseCode = "400",
+                  description = "Wrong type of verification code sent",
+                  content = @Content(
+                          mediaType = "application/json",
+                          schema = @Schema(implementation = IncorrectDefaultNotificationTypeErrorResponse.class))),
+          @ApiResponse(responseCode = "423",
+                  description = "The selected confirmation method is blocked. Choose another",
+                  content = @Content(
+                          mediaType = "application/json",
+                          schema = @Schema(implementation = DefaultConfirmationTypeLockedErrorResponse.class))),
+  })
   @PostMapping("/setDefaultConfirmationType")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "The default way to send a confirmation code has been changed.",
-            content =
-                @Content(
-                    schema = @Schema(example = "{\n" + "  \"personContact\": email\"\n" + "}\n"))),
-        @ApiResponse(
-            responseCode = "422",
-            description = "Invalid operation id",
-            content =
-                @Content(
-                    schema =
-                        @Schema(
-                            example =
-                                "{\n"
-                                    + "    \"message\": \"Not found operation with this id\",\n"
-                                    + "    \"error\": \"Unprocessable Entity\",\n"
-                                    + "    \"status\": \"422\"\n"
-                                    + "}\n"))),
-        @ApiResponse(
-            responseCode = "400",
-            description = "Wrong type of verification code send",
-            content =
-                @Content(
-                    schema =
-                        @Schema(
-                            example =
-                                "{\n"
-                                    + "    \"message\": \"Incorrect confirmation type\",\n"
-                                    + "    \"error\": \"Bad request\",\n"
-                                    + "    \"status\": \"400\"\n"
-                                    + "}\n"))),
-        @ApiResponse(
-            responseCode = "423",
-            description = "The selected confirmation method is blocked. Choose another",
-            content =
-                @Content(
-                    schema =
-                        @Schema(
-                            example =
-                                ". {\n"
-                                    + "    \"error\": \"Locked\",\n"
-                                    + "    \"status\": \"423\",\n"
-                                    + "   \"unlockTime\": \"time in ms\",\n"
-                                    + "}\n")))
-      })
   public SetDefaultConfirmationTypeResponse setDefaultType(
       @Valid @RequestBody SetDefaultNotificationTypeRequest setTypeRequestDto) {
 
     return notificationSettingsService.setPersonDefaultConfirmationType(setTypeRequestDto);
   }
 
-  @Operation(summary = "Get a summary of blocked methods for sending a confirmation code")
+  @Operation(summary = "Get all blocked confirmation methods",
+  description = "Get a summary of blocked methods for sending a confirmation code")
   @GetMapping("/getConfirmationLocks")
   @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "Returns a list with blocking statuses of ways to send code",
-            description = "OK",
-            content =
-                @Content(
-                    schema =
-                        @Schema(
-                            example =
-                                "{\n"
-                                    + "    \"email\", \"423123\",\n"
-                                    + "    \"push\", \"12344\"\n"
-                                    + "}\n"))),
-        @ApiResponse(
-            responseCode = "422",
-            description = "Invalid operation id",
-            content =
-                @Content(
-                    schema =
-                        @Schema(
-                            example =
-                                "{\n"
-                                    + "    \"message\": \"Not found operation with this id\",\n"
-                                    + "    \"error\": \"Unprocessable Entity\",\n"
-                                    + "    \"status\": \"422\"\n"
-                                    + "}\n")))
-      })
+          value = {
+                  @ApiResponse(responseCode = "200",
+                          description = "Returns a list with blocking statuses of ways to send code",
+                          content =
+                          @Content(
+                                  mediaType = "application/json",
+                                  schema = @Schema(implementation = Map.class,
+                                          example =
+                                                  "{\n"
+                                                          + "    \"email\", \"423123\",\n"
+                                                          + "    \"push\", \"12344\"\n"
+                                                          + "}\n"))),
+                  @ApiResponse(responseCode = "422",
+                          description = "Invalid operation id",
+                          content = @Content(
+                                  mediaType = "application/json",
+                                  // "message": "Not found operation with this id"
+                                  schema = @Schema(implementation = NotFoundErrorResponse.class))),
+          })
   public Map<String, String> getPersonLocks(String personId) {
 
     return notificationSettingsService.getPersonConfirmationLocks(UUID.fromString(personId));
